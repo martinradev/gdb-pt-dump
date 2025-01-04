0d4adfeebabc887549f4c5be415131cfe0580dd0
Martin Radev <martin.b.radev@gmail.com>
2025-01-04 18:15:36 +0200

    tests: don't restart gdb, use qemu monitor
    There are multiple changes done to make tests pass and be faster:
    - don't restart gdb, instead keep gdb connection up and issue new commands
    - use qemu monitor instead of qmp, since qmp does not offer a way to
      get the memory ranges.
    - skip checking translation correctness for IO and ROM regions
      since they may only cover a small part of a physical page, and
      the tests wouldn't know if the memory is actually intended to
      be accessible.

--- tests/integration_tests/vm_utils.py
+++ tests/integration_tests/vm_utils.py
@@ -8,6 +8,7 @@ import json
 from abc import ABC, abstractmethod
 
 _DEFAULT_QEMU_MONITOR_PORT=55555
+_DEFAULT_QEMU_OLD_MONITOR_PORT=54545
 
 # TODO: add images to github and write a downloader script
 class ImageContainer:
@@ -44,10 +45,11 @@ class ImageContainer:
             raise Exception(f"Unknown arch {arch_name}")
 
 class VM(ABC):
-    def __init__(self, arch, qemu_monitor_port):
+    def __init__(self, arch, qemu_monitor_port, qemu_old_monitor_port):
         self.vm_proc = None
         self.arch = arch
         self.qemu_monitor_port = qemu_monitor_port
+        self.qemu_old_monitor_port = qemu_old_monitor_port
         self.print_uart = bool(os.getenv("GDB_PT_DUMP_TESTS_PRINT_UART"))
 
     def start(self, cmd):
@@ -78,6 +80,9 @@ class VM(ABC):
 
     def get_qemu_monitor_port(self):
         return self.qemu_monitor_port
+
+    def get_qemu_old_monitor_port(self):
+        return self.qemu_old_monitor_port
 
     def wait_for_string_on_line(self, string):
         line = b""
@@ -111,6 +116,7 @@ class VM(ABC):
     def stop(self):
         if self.vm_proc:
             self.vm_proc.kill()
+            self.vm_proc.wait()
             self.vm_proc = None
 
     def is_alive(self):
@@ -120,7 +126,7 @@ class VM(ABC):
 
 class VM_X86_64(VM):
     def __init__(self, image_dir, fda_name=None):
-        super().__init__(arch="x86_64", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT)
+        super().__init__(arch="x86_64", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT, qemu_old_monitor_port=_DEFAULT_QEMU_OLD_MONITOR_PORT)
         self.image_dir = image_dir
         self.fda_name = fda_name
 
@@ -174,6 +180,8 @@ class VM_X86_64(VM):
 
         cmd.extend(["-qmp", f"tcp:localhost:{self.get_qemu_monitor_port()},server,nowait"])
 
+        cmd.extend(["-monitor", f"tcp:localhost:{self.get_qemu_old_monitor_port()},server,nowait"])
+
         cmd.extend(["-nographic", "-snapshot", "-no-reboot"])
 
         cmd.extend(["-smp", str(num_cores)])
@@ -196,7 +204,7 @@ class VM_X86_64(VM):
 
 class VM_X86_32(VM):
     def __init__(self, image_dir, fda_name=None):
-        super().__init__(arch="x86_32", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT)
+        super().__init__(arch="x86_32", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT, qemu_old_monitor_port=_DEFAULT_QEMU_OLD_MONITOR_PORT)
         self.image_dir = image_dir
         self.fda_name = fda_name
 
@@ -226,6 +234,8 @@ class VM_X86_32(VM):
 
         cmd.extend(["-qmp", f"tcp:localhost:{self.get_qemu_monitor_port()},server,nowait"])
 
+        cmd.extend(["-monitor", f"tcp:localhost:{self.get_qemu_old_monitor_port()},server,nowait"])
+
         cmd.extend(["-nographic", "-snapshot", "-no-reboot"])
 
         cmd.extend(["-smp", str(num_cores)])
@@ -248,7 +258,7 @@ class VM_X86_32(VM):
 
 class VM_Arm_64(VM):
     def __init__(self, image_dir, bios_name=None, has_kernel=True):
-        super().__init__(arch="arm_64", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT)
+        super().__init__(arch="arm_64", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT, qemu_old_monitor_port=_DEFAULT_QEMU_OLD_MONITOR_PORT)
         self.image_dir = image_dir
         self.bios_name = bios_name
         self.has_kernel = has_kernel
@@ -285,6 +295,8 @@ class VM_Arm_64(VM):
 
         cmd.extend(["-qmp", f"tcp:localhost:{self.get_qemu_monitor_port()},server,nowait"])
 
+        cmd.extend(["-monitor", f"tcp:localhost:{self.get_qemu_old_monitor_port()},server,nowait"])
+
         cmd.extend(["-nographic", "-snapshot", "-no-reboot"])
 
         cmd.extend(["-smp", str(num_cores)])
@@ -293,6 +305,8 @@ class VM_Arm_64(VM):
 
         super().start(cmd)
 
+    # TODO
+    # The addresses are not correct when LA57 is enabled
     def get_default_base_image_kaddr(self):
         return [0xffff800010000000, 0xffffffc008010000, 0xfffffe0008010000]
 
@@ -307,7 +321,7 @@ class VM_Arm_64(VM):
 
 class VM_Riscv(VM):
     def __init__(self, image_dir):
-        super().__init__(arch="riscv", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT)
+        super().__init__(arch="riscv", qemu_monitor_port=_DEFAULT_QEMU_MONITOR_PORT, qemu_old_monitor_port=_DEFAULT_QEMU_OLD_MONITOR_PORT)
         self.image_dir = image_dir
 
     def start(self, memory_mib=64, kvm=False, kaslr=True, num_cores=1):
@@ -342,6 +356,8 @@ class VM_Riscv(VM):
 
         cmd.extend(["-qmp", f"tcp:localhost:{self.get_qemu_monitor_port()},server,nowait"])
 
+        cmd.extend(["-monitor", f"tcp:localhost:{self.get_qemu_old_monitor_port()},server,nowait"])
+
         cmd.extend(["-nographic", "-snapshot", "-no-reboot"])
 
         cmd.extend(["-smp", str(num_cores)])
@@ -361,6 +377,131 @@ class VM_Riscv(VM):
 
     def get_fixed_known_address(self):
         raise Exception("Unimplemented")
+
+class FlatViewRange:
+
+    def __init__(self, range_start, range_end, range_type):
+        self.range_start = range_start
+        self.range_end = range_end
+        self.range_type = range_type
+
+    def is_memory_backed(self):
+        return self.range_type in ["ram"]
+
+    def is_rom(self):
+        return self.range_type in ["rom"]
+
+    def is_io(self):
+        return self.range_type in ["i/o"]
+
+    def __str__(self):
+        return f"VA_start: {hex(self.range_start)}, VA_end: {hex(self.range_end)}, Type: {self.range_type}"
+
+class VmFlatView:
+
+    def __init__(self, tree_data):
+        self._tree_data = tree_data
+        self._ranges = []
+
+        pattern = "^([0-9a-f]{16})-([0-9a-f]{16}) \\(prio \d, (.+)\\): (.+)$"
+        res = ""
+        for line in tree_data.split("\n"):
+            line = line.strip()
+            matching = re.match(pattern, line)
+            if matching:
+                memory_type = matching.group(3)
+                if memory_type in ["ram", "rom"]:
+                    range_start = int(matching.group(1), 16)
+                    range_end = int(matching.group(2), 16) + 1
+                    self._ranges.append(FlatViewRange(range_start, range_end, memory_type))
+
+    def find_range(self, pa):
+        for r in self._ranges:
+            if pa >= r.range_start and pa < r.range_end:
+                return r
+        return None
+
+    def find_prev_range(self, pa):
+        prev = None
+        for r in self._ranges:
+            if r.range_end <= pa:
+                prev = r
+        return prev
+
+class QemuOldMonitorExecutor:
+
+    def __init__(self, vm):
+        self.qemu_monitor_port = vm.get_qemu_old_monitor_port()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(("localhost", self.qemu_monitor_port))
+        self._read_until("(qemu)")
+
+    def stop(self):
+        if self.socket:
+            self.socket.shutdown(socket.SHUT_WR)
+            self.socket.close()
+
+    def _read_until(self, until):
+        buf = ""
+        while True:
+            b = self.socket.recv(1)
+            if b != b'':
+                buf += b.decode()
+            if buf.endswith(until):
+                break
+        return buf
+
+    def _send_command(self, command):
+        self.socket.send(command.encode() + b"\n")
+        res = self._read_until("(qemu)")[:-7]
+        res = res[res.find("\n"):]
+        return res
+
+    def get_memory_flat_view(self):
+        tree = self._send_command("info mtree -f")
+        return VmFlatView(tree)
+
+    def gva2gpa(self, addr):
+        res = self._send_command(f"gva2gpa {hex(addr)}").strip()
+        matching = re.match("gpa: (.+)", res)
+        if matching == None:
+            return None
+        res = matching.group(1)
+        gpa_addr = int(res, 16)
+        return gpa_addr
+
+    def read_virt_memory(self, addr, len):
+        data = self._read_memory(addr, len, True)
+        return data
+
+    def _read_memory(self, addr, len, is_virt):
+        exec = ""
+        if is_virt:
+            exec = "memsave"
+        else:
+            exec = "pmemsave"
+        filename = "/tmp/tmp.bin"
+        if os.path.isfile(filename):
+            os.remove(filename)
+
+        self._send_command(f"{exec} {addr} {len} \"{filename}\"")
+
+        data = b""
+        for u in range(3):
+            try:
+                with open(filename, "rb") as f:
+                    data = f.read()
+                    break
+            except:
+                time.sleep(0.1)
+        os.remove(filename)
+        return data
+
+    def pause(self):
+        self._send_command("stop")
+
+    def resume(self):
+        self._send_command("cont")
 
 class QemuMonitorExecutor:
 
@@ -462,7 +603,20 @@ class GdbCommandExecutor:
         self.use_multiarch = vm.get_arch() != "x86_64"
         self.script_root_pt = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "../", "pt.py"))
 
-    def run_cmd(self, pt_cmd):
+        # Start a GDB process immediately so that
+        self._start_gdb_process()
+
+    def __del__(self):
+        self.stop()
+
+    def stop(self):
+        if self._subproc:
+            self._subproc.kill()
+            self._subproc.wait()
+            self._subproc = None
+
+    def _start_gdb_process(self):
+        _launch_message = "GDB server launch done"
         cmd = []
         if self.use_multiarch:
             cmd.extend(["gdb-multiarch"])
@@ -475,15 +629,34 @@ class GdbCommandExecutor:
         cmd.extend(["-ex", "set pagination off"])
         cmd.extend(["-ex", "source {}".format(self.script_root_pt)])
         cmd.extend(["-ex", f"target remote :{self.gdb_server_port}"])
-        cmd.extend(["-ex", pt_cmd])
-        cmd.extend(["-ex", "quit"])
+
+        self._subproc = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True, bufsize=0)
+
+    def _read_until(self, until_str):
+        buf = ""
+        while True:
+            b = self._subproc.stdout.read(1)
+            if b != "":
+                buf += b
+            if buf.endswith(until_str):
+                break
+        return buf
+
+    def run_cmd(self, pt_cmd):
+        if not self._subproc:
+            self._start_gdb_process()
+
         t1 = time.time()
-        result = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode("utf-8")
+        self._read_until("(gdb)")
+        self._subproc.stdin.write(pt_cmd + "\n")
+        self._subproc.stdin.write("p \"(DONE)\"\n")
+        res = self._read_until("(DONE)")[:-7]
         t2 = time.time()
-        if "error" in result.lower() or "exception" in result.lower():
-            raise Exception(f"Executuing command failed: '{result}'")
+
+        if "error" in res.lower() or "exception" in res.lower():
+            raise Exception(f"Executuing command failed: '{res}'")
         elapsed = t2 - t1
-        return GdbCommandExecutor.Result(result, elapsed)
+        return GdbCommandExecutor.Result(res, elapsed)
 
 def create_linux_vm(arch_name, image_name = None):
     if arch_name == "x86_64":
@@ -517,4 +690,56 @@ def get_arm_64_binary_names():
     # Filter out 16k granule
     files = [file for file in files if "16k" not in file]
     return files
+
+def check_va_exists(old_monitor, monitor, flatview, va):
+    data = old_monitor.read_virt_memory(va, 4)
+    if len(data) == 4:
+        # This is the common case that the memory is accessible
+        return True
+
+    pa = old_monitor.gva2gpa(va)
+    if pa == None:
+        print("Qemu failed to translate the GVA altogether")
+        print("This probably means that the page-table parsing or range merging is incorrect")
+        return False
+
+    r = flatview.find_range(pa)
+    if r == None:
+        if prev_range := flatview.find_prev_range(va):
+            if prev_range.is_io() or prev_range.is_rom():
+                page_aligned_pa = pa & 0xFF_FF_FF_FF_FF_FF_F0_00
+                if page_aligned_pa < prev_range.range_end:
+                    return True
+        print(f"Failed to find the PA ({hex(pa)}) in the collected flatview ranges")
+        print("This can mean the whole page is part of IO/ROM and only part of the address is accessible")
+        return False
+
+    if r.is_io():
+        # If it's not IO, it should be either RAM or ROM.
+        assert(r.is_memory_backed())
+
+        # IO may only implement part of the physical memory range to be accessible, so accesses will fail
+        # even if technically the physical page exists.
+        return True
+
+    # Anything else that's not handled is a failure to access memory
+    return False
+
+def check_if_belongs_to_io_or_rom(old_monitor, flatview, va):
+    pa = old_monitor.gva2gpa(va)
+    if pa == None:
+        print("Qemu failed to translate the GVA altogether")
+        print("This probably means that the page-table parsing or range merging is incorrect")
+        return False
+
+    if pa == None:
+        print("Qemu failed to translate the GVA altogether")
+        print("This probably means that the page-table parsing or range merging is incorrect")
+        return False
+
+    r = flatview.find_range(pa)
+    if r != None:
+        return r.is_io() or r.is_rom()
+
+    return False
 
