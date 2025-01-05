4a67b2c6a9b8d9751fb26defb0557c69c64f879f
Martin Radev <martin.b.radev@gmail.com>
2025-01-05 21:56:28 +0200

    tests: fix pytest skip
    pytest.skip somehow does not accept keyword-argument within
    the docker environment for some reason. The fix is to pass
    the skip reason as a positional (first) argument.

--- tests/integration_tests/run_integration_tests.py
+++ tests/integration_tests/run_integration_tests.py
@@ -276,7 +276,7 @@ def test_pt_filter_read_only(create_resources_fixture, arch_name, linux_image):
 def test_pt_filter_user_accessible(create_resources_fixture, arch_name, linux_image):
     if arch_name == "arm_64" and "kpti" in linux_image:
         # BUG: needs another kernel image
-        pytest.skip(reason = "User ranges are never visible because user page table is unmapped")
+        pytest.skip("User ranges are never visible because user page table is unmapped")
     vm, gdb, monitor, memory_flatview = create_resources_fixture
     executions = [("pt -filter u", lambda r: r.flags.user_accessible())]
     _test_pt_filter_common(vm, gdb, monitor, executions)
@@ -285,7 +285,7 @@ def test_pt_filter_user_accessible(create_resources_fixture, arch_name, linux_im
 def test_pt_filter_kernel_only_accessible(create_resources_fixture, arch_name, linux_image):
     if arch_name == "arm_64" and "kpti" in linux_image:
         # BUG: needs another kernel image
-        pytest.skip(reason = "The _s would result into 0 ranges because user page table is unmapped.")
+        pytest.skip("The _s would result into 0 ranges because user page table is unmapped.")
     executions = [("pt -filter s", lambda r: r.flags.super_accessible())]
     vm, gdb, monitor, memory_flatview = create_resources_fixture
     _test_pt_filter_common(vm, gdb, monitor, executions)
@@ -294,7 +294,7 @@ def test_pt_filter_kernel_only_accessible(create_resources_fixture, arch_name, l
 def test_pt_filter_multiple_filters_user(create_resources_fixture, arch_name, linux_image):
     if arch_name == "arm_64" and "kpti" in linux_image:
         # BUG: needs another kernel image
-        pytest.skip(reason = "This would result into 0 ranges because user page table is unmapped.")
+        pytest.skip("This would result into 0 ranges because user page table is unmapped.")
     executions = [ \
                   ("pt -filter w u", lambda r: r.flags.user_writeable()), \
                  ]
@@ -413,7 +413,7 @@ def test_pt_has_command(create_resources_fixture, arch_name, linux_image):
 def test_pt_before_command(create_resources_fixture, arch_name, linux_image):
     if arch_name == "arm_64":
         # BUG: needs gdb_pt_dump fix
-        pytest.skip(reason = "The gdb_pt_dump aarch64 backend does not implement cut_before and cut_after.")
+        pytest.skip("The gdb_pt_dump aarch64 backend does not implement cut_before and cut_after.")
 
     vm, gdb, monitor, memory_flatview = create_resources_fixture
 
@@ -465,7 +465,7 @@ def test_pt_before_command(create_resources_fixture, arch_name, linux_image):
 def test_pt_after_command(create_resources_fixture, arch_name, linux_image):
     if arch_name == "arm_64":
         # BUG: needs gdb_pt_dump fix
-        pytest.skip(reason = "The gdb_pt_dump aarch64 backend does not implement cut_before and cut_after.")
+        pytest.skip("The gdb_pt_dump aarch64 backend does not implement cut_before and cut_after.")
 
     vm, gdb, monitor, memory_flatview = create_resources_fixture
 
@@ -496,7 +496,7 @@ def test_pt_after_command(create_resources_fixture, arch_name, linux_image):
 def test_pt_before_after_combination(create_resources_fixture, arch_name, linux_image):
     if arch_name == "arm_64":
         # BUG: needs gdb_pt_dump fix
-        pytest.skip(reason = "The gdb_pt_dump aarch64 backend does not implement cut_before and cut_after.")
+        pytest.skip("The gdb_pt_dump aarch64 backend does not implement cut_before and cut_after.")
 
     vm, gdb, monitor, memory_flatview = create_resources_fixture
 
@@ -533,9 +533,9 @@ def test_pt_before_after_combination(create_resources_fixture, arch_name, linux_
 @pytest.mark.parametrize("arch_name, linux_image", get_all_images())
 def test_pt_kaslr(create_resources_fixture_nokaslr, arch_name, linux_image):
     if arch_name == "riscv":
-        pytest.skip(reason = "KASLR commands not supported with riscv")
+        pytest.skip("KASLR commands not supported with riscv")
     if "la57" in linux_image:
-        pytest.skip(reason = "The test needs to be updated to have the correct hardcoded base addresses when LA57 is enabled")
+        pytest.skip("The test needs to be updated to have the correct hardcoded base addresses when LA57 is enabled")
     virt_pattern = re.compile(r'Virt:\s+([0-9a-fA-Fx]+)')
     phys_pattern = re.compile(r'Phys:\s+([0-9a-fA-Fx]+)')
 
@@ -581,7 +581,7 @@ def get_custom_binaries():
 @pytest.fixture
 def create_custom_resources_fixture(arch_name, image_name):
     if arch_name == "arm_64" and are_arm64_custom_images_broken():
-        pytest.skip(reason = "Custom images are broken on QEMU Aarch64 8.2.2 (VM faults)")
+        pytest.skip("Custom images are broken on QEMU Aarch64 8.2.2 (VM faults)")
     vm = create_custom_vm(arch_name, image_name)
     vm.start()
     vm.wait_for_string_on_line(b"Done")
@@ -620,7 +620,7 @@ def test_golden_images(request, create_custom_resources_fixture, arch_name, imag
 @pytest.mark.parametrize("arch_name, image_name", get_custom_binaries())
 def test_phys_verbose_golden_images(request, create_custom_resources_fixture, arch_name, image_name):
     if arch_name == "arm_64":
-        pytest.skip(reason = "phys verbose not correct for arm64")
+        pytest.skip("phys verbose not correct for arm64")
     vm, gdb, monitor, flatview = create_custom_resources_fixture
     test_name = request.node.name
     generated_image_name = "/tmp/.gdb_pt_dump_phys_verbose_{}".format(image_name)
@@ -641,7 +641,7 @@ def test_phys_verbose_golden_images(request, create_custom_resources_fixture, ar
 @pytest.mark.parametrize("arch_name, image_name", get_custom_binaries())
 def test_pt_walk_golden_images(request, create_custom_resources_fixture, arch_name, image_name):
     if arch_name == "arm_64":
-        pytest.skip(reason = "golden images are not present")
+        pytest.skip("golden images are not present")
     vm, gdb, monitor, flatview = create_custom_resources_fixture
     test_name = request.node.name
     generated_image_name = "/tmp/.gdb_pt_dump_pt_walk_{}".format(image_name)
@@ -726,7 +726,7 @@ def test_pt_i386():
 @pytest.mark.parametrize("arch_name, linux_image", get_all_images())
 def test_pt_read_virt_memory(create_resources_fixture_nokaslr, arch_name, linux_image):
     if arch_name == "arm_64":
-        pytest.skip(reason = "pt command fails on arm_64 for some reason, needs debugging")
+        pytest.skip("pt command fails on arm_64 for some reason, needs debugging")
     vm, gdb, monitor, memory_flatview = create_resources_fixture_nokaslr
 
     res = gdb.run_cmd("pt")
